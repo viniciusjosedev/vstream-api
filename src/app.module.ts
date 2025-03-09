54c9d7cf658d20ba74f048dbfaf3ddaa9d217140
Vinicius José <viniciusjosedev@gmail.com>
2025-03-09 15:44:51 -0300

    feat: videoModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { VideoModule } from './modules/videos/video.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, VideoModule],
   controllers: [],
   providers: [],
 })
