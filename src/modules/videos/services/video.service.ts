9d2615d522397f7c6daba5c20d246c32eba26834
Vinicius José <viniciusjosedev@gmail.com>
2025-03-09 18:45:42 -0300

    feat: add agent with cookies

--- src/modules/videos/services/video.service.ts
+++ src/modules/videos/services/video.service.ts
@@ -1,11 +1,18 @@
 import * as ytdl from '@distube/ytdl-core';
+import { variablesEnv } from 'src/envs/variables.env';
 
 export class VideoService {
+  private readonly agent: ytdl.Agent;
+
+  constructor() {
+    this.agent = ytdl.createAgent(JSON.parse(variablesEnv.cookies));
+  }
+
   public async getFormats(url: string) {
-    return (await ytdl.getInfo(url)).formats;
+    return (await ytdl.getInfo(url, { agent: this.agent })).formats;
   }
 
   public async getInfo(url: string) {
-    return ytdl.getBasicInfo(url);
+    return ytdl.getBasicInfo(url, { agent: this.agent });
   }
 }
