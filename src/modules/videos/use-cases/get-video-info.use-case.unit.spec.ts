9dd0ca98e60fd333fed49659f4e80ea51505294c
Vinicius José <viniciusjosedev@gmail.com>
2025-03-13 14:43:04 -0300

    feat: add test to get-video-info.use-case

---
+++ src/modules/videos/use-cases/get-video-info.use-case.unit.spec.ts
@@ -0,0 +1,112 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GetVideoInfo } from '../use-cases/get-video-info.use-case';
+import { VideoService } from '../services/video.service';
+import { ValidFields } from '../dto/get-info.controller.dto';
+
+jest.mock('@nestjs/jwt', () => {
+  const actual = jest.requireActual('@nestjs/jwt');
+
+  return {
+    ...actual,
+    JwtService: jest.fn().mockImplementation(() => {
+      return {
+        sign: jest.fn().mockReturnValue('token'),
+      };
+    }),
+  };
+});
+
+describe('GetVideoInfo', () => {
+  let getVideoInfo: GetVideoInfo;
+  let videoService: VideoService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [],
+      controllers: [],
+      providers: [GetVideoInfo, VideoService],
+    }).compile();
+
+    getVideoInfo = app.get<GetVideoInfo>(GetVideoInfo);
+    videoService = app.get<VideoService>(VideoService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return token', async () => {
+    const getVideoInfoMock = {
+      channel: {
+        channel_url: 'channel_url',
+        name: 'name',
+        photo_url: 'avatar_url',
+      },
+      formats: [
+        {
+          format: 'video/mp4',
+          hasAudio: true,
+          hasVideo: true,
+          qualityAudio: 'medium',
+          qualityVideo: '720p',
+          url: 'url',
+        },
+      ],
+      thumbnail: {
+        height: 1080,
+        width: 1920,
+        url: 'url',
+      },
+      title: 'title',
+    };
+
+    const getInfoSpy = jest.spyOn(videoService, 'getInfo').mockResolvedValue({
+      videoDetails: {
+        title: 'title',
+        author: {
+          channel_url: 'channel_url',
+          name: 'name',
+          avatar: 'avatar_url',
+        },
+        thumbnails: [
+          {
+            height: 1080,
+            width: 1920,
+            url: 'url',
+          },
+        ],
+      },
+    } as any);
+
+    const getFormatsSpy = jest
+      .spyOn(videoService, 'getFormats')
+      .mockResolvedValue([
+        {
+          hasAudio: true,
+          hasVideo: true,
+          qualityLabel: '720p',
+          quality: 'medium',
+          url: 'url',
+          mimeType: 'video/mp4; codcs',
+        },
+      ] as any);
+
+    expect(
+      await getVideoInfo.execute({
+        fields: [
+          ValidFields.TITLE,
+          ValidFields.CHANNEL,
+          ValidFields.THUMBNAILS,
+          ValidFields.FORMATS,
+        ],
+        url: 'url',
+      }),
+    ).toStrictEqual(getVideoInfoMock);
+
+    expect(getInfoSpy).toHaveBeenCalled();
+    expect(getInfoSpy).toHaveBeenCalledTimes(1);
+
+    expect(getFormatsSpy).toHaveBeenCalled();
+    expect(getFormatsSpy).toHaveBeenCalledTimes(1);
+  });
+});
