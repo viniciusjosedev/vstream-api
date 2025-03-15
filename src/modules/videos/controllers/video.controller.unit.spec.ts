899e8198418e9cad29e969c7e79b25af1b4eac71
Vinicius José <viniciusjosedev@gmail.com>
2025-03-15 14:11:44 -0300

    feat: add video.controller.unit to VideoController

--- src/modules/videos/controllers/video.controller.unit.spec.ts
+++ src/modules/videos/controllers/video.controller.unit.spec.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 import { ValidFields } from '../dto/get-info.controller.dto';
 import { VideoService } from '../services/video.service';
 import { GetVideoUseCase } from '../use-cases/get-video.use-case';
+import { Response } from 'express';
 
 jest.mock('@nestjs/jwt', () => {
   const actual = jest.requireActual('@nestjs/jwt');
@@ -22,6 +23,7 @@ jest.mock('@nestjs/jwt', () => {
 describe('VideoController', () => {
   let videoController: VideoController;
   let getVideoInfoUseCase: GetVideoInfoUseCase;
+  let getVideoUseCase: GetVideoUseCase;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -37,54 +39,72 @@ describe('VideoController', () => {
 
     videoController = app.get<VideoController>(VideoController);
     getVideoInfoUseCase = app.get<GetVideoInfoUseCase>(GetVideoInfoUseCase);
+    getVideoUseCase = app.get<GetVideoUseCase>(GetVideoUseCase);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should return video infos', async () => {
-    const getVideoInfoMock = {
-      channel: {
-        channel_url: 'url',
-        name: 'name',
-        photo_url: 'url',
-      },
-      formats: [
-        {
-          format: 'video/mp4',
-          hasAudio: true,
-          hasVideo: true,
-          qualityAudio: 'medium',
-          qualityVideo: '720p',
+  describe('getInfo method', () => {
+    it('should return video infos', async () => {
+      const getVideoInfoMock = {
+        channel: {
+          channel_url: 'url',
+          name: 'name',
+          photo_url: 'url',
+        },
+        formats: [
+          {
+            format: 'video/mp4',
+            hasAudio: true,
+            hasVideo: true,
+            qualityAudio: 'medium',
+            qualityVideo: '720p',
+            url: 'url',
+          },
+        ],
+        thumbnail: {
+          height: 1080,
+          width: 1920,
           url: 'url',
         },
-      ],
-      thumbnail: {
-        height: 1080,
-        width: 1920,
-        url: 'url',
-      },
-      title: 'title',
-    };
+        title: 'title',
+      };
 
-    const getVideoInfoUseCaseSpy = jest
-      .spyOn(getVideoInfoUseCase, 'execute')
-      .mockResolvedValue(getVideoInfoMock);
+      const getVideoInfoUseCaseSpy = jest
+        .spyOn(getVideoInfoUseCase, 'execute')
+        .mockResolvedValue(getVideoInfoMock);
 
-    expect(
-      await videoController.getInfo({
-        fields: [
-          ValidFields.TITLE,
-          ValidFields.CHANNEL,
-          ValidFields.THUMBNAIL,
-          ValidFields.FORMATS,
-        ],
-        url: 'url',
-      }),
-    ).toStrictEqual(getVideoInfoMock);
+      expect(
+        await videoController.getInfo({
+          fields: [
+            ValidFields.TITLE,
+            ValidFields.CHANNEL,
+            ValidFields.THUMBNAIL,
+            ValidFields.FORMATS,
+          ],
+          url: 'url',
+        }),
+      ).toStrictEqual(getVideoInfoMock);
 
-    expect(getVideoInfoUseCaseSpy).toHaveBeenCalled();
-    expect(getVideoInfoUseCaseSpy).toHaveBeenCalledTimes(1);
+      expect(getVideoInfoUseCaseSpy).toHaveBeenCalled();
+      expect(getVideoInfoUseCaseSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getVideo method', () => {
+    it('should return undefined', async () => {
+      const getVideoUseCaseSpy = jest
+        .spyOn(getVideoUseCase, 'execute')
+        .mockResolvedValue();
+
+      expect(
+        await videoController.getVideo({ url: 'url' }, {} as Response),
+      ).toBeUndefined();
+
+      expect(getVideoUseCaseSpy).toHaveBeenCalled();
+      expect(getVideoUseCaseSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
