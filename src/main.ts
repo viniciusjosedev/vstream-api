fcbb3d071a40bdd18eb215c053f0a7231602ded8
Vinicius José <viniciusjosedev@gmail.com>
2025-03-15 15:21:02 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/response';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
