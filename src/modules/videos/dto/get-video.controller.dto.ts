644f067ac02715fc68048b4d0f284e94ae6fe5f7
Vinicius José <viniciusjosedev@gmail.com>
2025-03-18 11:21:34 -0300

    feat: add decodeURIComponent in transform

--- src/modules/videos/dto/get-video.controller.dto.ts
+++ src/modules/videos/dto/get-video.controller.dto.ts
@@ -1,8 +1,14 @@
 import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class GetVideoControllerDTO {
   @ApiProperty({ required: true })
+  @Transform((value) => {
+    return typeof value.value === 'string'
+      ? decodeURIComponent(value.value)
+      : value;
+  })
   @IsNotEmpty()
   url: string;
 }
